2feeb6ee29d988bd4a26b1d32c2ca17ff0932f49
DonatoAletta <donatoalettawork@gmail.com>
2024-10-29 10:17:00 +0100

    Initial commit

---
+++ src/main/java/com/donato/training/ishalldo/IShallDoApplication.java
@@ -0,0 +1,13 @@
+package com.donato.training.ishalldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IShallDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IShallDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donato/training/ishalldo/IShallDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donato.training.ishalldo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IShallDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
